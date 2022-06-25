8be044a1a8cc15e11704ad00ed9d68775eb7db3a
Daniela Mera <daniela.antonieta@gmail.com>
2022-06-25 00:10:30 -0400

    creacion licitaciones, vistas y controlador

--- app/models/chileregion.rb
+++ app/models/chileregion.rb
@@ -1,6 +1,15 @@
 class Chileregion < ApplicationRecord
-  #Relacion 1-n
-  has_many :user, class_name:"User", foreign_key:"user_id"
+  #relacion 1-n
+  #Un usuario debe tener una comuna,region,tipoentidad
+  #Una comuna,region,tipoentidad puede tener muchos usuarios
+  has_many :users, class_name:"User", foreign_key:"user_id"
   #belongs_to :user, class_name:"User", foreign_key:"user_id"
+  
+  #relacion 1-n
+  #Una region puede tener muchas licitaciones
+  #Una licitacion debe tener una region
+  has_many :licitacions, class_name:"Licitacion", foreign_key:"user_id"
+  
+
 
 end
