7d8e0e3ecca0d41aa056b3b3cfa6a086ddecd682
Daniela Mera <daniela.antonieta@gmail.com>
2022-06-24 22:51:39 -0400

    Relacion 1n user regiones comunas tipoentidad

--- app/models/tipo_entidad.rb
+++ app/models/tipo_entidad.rb
@@ -1,2 +1,6 @@
 class TipoEntidad < ApplicationRecord
+  #Relacion 1-n
+  has_many :user, class_name:"User", foreign_key:"user_id"
+  #belongs_to :user, class_name:"User", foreign_key:"user_id"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #relacion 1-n
+
+  #Un usuario debe tener una comuna,region,tipoentidad
+  belongs_to :comuna, class_name:"Comuna", foreign_key:"nombre_comuna"
+  belongs_to :chileregion, class_name:"Chileregion", foreign_key:"nombre_region"
+  belongs_to :tipo_entidad, class_name:"TipoEntidad", foreign_key:"entidad"
+
+  #Un usuario debe tener una comuna,region,tipoentidad
+  #has_many :comuna, class_name:"Comuna", foreign_key:"nombre_comuna"
+  #has_many :chileregion, class_name:"Chileregion", foreign_key:"nombre_region"
+  #has_many :tipo_entidad, class_name:"TipoEntidad", foreign_key:"entidad"
+
+
+
 end
